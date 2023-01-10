5ac5ab6e3f75f86e9d6cd41c6612b6d372ceb628
luyifei <cathow@163.com>
2023-01-10 13:54:02 +0800

    695 ms

---
+++ src/main/java/leetcode/m695.java
@@ -0,0 +1,70 @@
+package leetcode;
+
+/**
+ * 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在 水平或者竖直的四个方向上 相邻。你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+ *
+ * 岛屿的面积是岛上值为 1 的单元格的数目。
+ *
+ * 计算并返回 grid 中最大的岛屿面积。如果没有岛屿，则返回面积为 0 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/max-area-of-island
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 输入：grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * 输出：6
+ * 解释：答案不应该是 11 ，因为岛屿只能包含水平或垂直这四个方向上的 1 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/max-area-of-island
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class m695 {
+    public static int maxAreaOfIsland(int[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0;j<grid[i].length;j++){
+                res = Math.max(res,dfs(grid,i,j));
+            }
+        }
+        return res;
+    }
+
+    public static int dfs(int[][] grid,int i,int j){
+        int max = 0;
+        if(i<0||j<0){
+            return 0;
+        }
+        if (i>=grid.length){
+            return 0;
+        }
+        if (j>=grid[i].length){
+            return 0;
+        }
+        if(grid[i][j]==0){
+            return 0;
+        }
+        grid[i][j]=0;
+        max += 1;
+        max += dfs(grid,i-1,j)+dfs(grid,i+1,j)+dfs(grid,i,j+1)+dfs(grid,i,j-1);
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {{0,0,1,0,0,0,0,1,0,0,0,0,0},
+                        {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                        {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                        {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                        {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                        {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                        {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                        {0,0,0,0,0,0,0,1,1,0,0,0,0}};
+//        System.out.println(grid[0][2]);
+        System.out.println(maxAreaOfIsland(grid));
+
+
+    }
+
+
+}
